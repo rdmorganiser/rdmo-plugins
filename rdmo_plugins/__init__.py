29c88e45ecf0a45285986a01a94d40c43b87194b
Jochen Klar <mail@jochenklar.de>
2020-08-04 11:11:38 +0200

    Add setup.py

---
+++ rdmo_plugins/__init__.py
@@ -0,0 +1,8 @@
+__title__ = 'rdmo-plugins'
+__version__ = '1.0.0'
+__author__ = 'Jochen Klar'
+__email__ = 'mail@jochenklar.de'
+__license__ = 'Apache-2.0'
+__copyright__ = 'Copyright 2020 RDMO-Arbeitsgemeinschaft'
+
+VERSION = __version__
---
+++ setup.py
@@ -0,0 +1,29 @@
+import re
+
+from setuptools import find_packages, setup
+
+# get metadata from mudule using a regexp
+with open('rdmo_plugins/__init__.py') as f:
+    metadata = dict(re.findall(r'__(.*)__ = [\']([^\']*)[\']', f.read()))
+
+setup(
+    name=metadata['title'],
+    version=metadata['version'],
+    author=metadata['author'],
+    author_email=metadata['email'],
+    maintainer=metadata['author'],
+    maintainer_email=metadata['email'],
+    license=metadata['license'],
+    url='https://github.com/rdmorganiser/rdmo-plugins',
+    description=u'RDMO is a tool to support the systematic planning, organisation and implementation of the data management throughout the course of a research project.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.5'
+    ],
+    packages=find_packages(),
+    include_package_data=True
+)
