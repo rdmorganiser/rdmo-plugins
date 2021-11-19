7365b0eb7b3ee0cc4dc7c5a05721f209c7d59400
Jochen Klar <mail@jochenklar.de>
2021-11-19 15:46:02 +0100

    Fix Oauth flow and store radar_id and radar_url

--- rdmo_plugins/exports/radar/providers.py
+++ rdmo_plugins/exports/radar/providers.py
@@ -2,10 +2,12 @@ import time
 
 from django import forms
 from django.conf import settings
-from django.shortcuts import redirect, reverse, render
+from django.shortcuts import redirect, render
 from django.utils.translation import gettext_lazy as _
 
+from rdmo.domain.models import Attribute
 from rdmo.projects.exports import Export
+from rdmo.projects.models import Value
 from rdmo.services.providers import OauthProviderMixin
 
 from .mixins import RadarMixin
@@ -27,14 +29,23 @@ class RadarExportProvider(RadarMixin, Export, OauthProviderMixin):
             self.fields['workspace'].widget = forms.RadioSelect(choices=workspace_choices)
 
     def render(self):
-        url = self.get_get_url()
-
         datasets = self.get_set('project/dataset/id')
         dataset_choices = [(dataset.set_index, dataset.value) for dataset in datasets]
 
         self.store_in_session(self.request, 'dataset_choices', dataset_choices)
+        self.store_in_session(self.request, 'project_id', self.project.id)
 
-        return self.get(self.request, url)
+        if self.pop_from_session(self.request, 'get') is True:
+            workspace_choices = self.get_from_session(self.request, 'workspace_choices')
+            form = self.Form(
+                dataset_choices=dataset_choices,
+                workspace_choices=workspace_choices
+            )
+            return render(self.request, 'plugins/exports_radar.html', {'form': form}, status=200)
+        else:
+            # run the oauth get request to obtain the workspace_choices
+            url = self.get_get_url()
+            return self.get(self.request, url)
 
     def submit(self):
         dataset_choices = self.get_from_session(self.request, 'dataset_choices')
@@ -46,33 +57,88 @@ class RadarExportProvider(RadarMixin, Export, OauthProviderMixin):
             workspace_choices=workspace_choices
         )
 
+        if 'cancel' in self.request.POST:
+            return redirect('project', self.project.id)
+
         if form.is_valid():
+            self.store_in_session(self.request, 'set_index', form.cleaned_data['dataset'])
+
             url = self.get_post_url(form.cleaned_data['workspace'])
             data = self.get_post_data(form.cleaned_data['dataset'])
             return self.post(self.request, url, data)
         else:
             return render(self.request, 'plugins/exports_radar.html', {'form': form}, status=200)
 
+    def get_get_url(self):
+        return '{}/radar/api/workspaces'.format(self.radar_url)
+
     def get_success(self, request, response):
-        dataset_choices = self.get_from_session(request, 'dataset_choices')
         workspace_choices = [
             (workspace.get('id'), workspace.get('descriptiveMetadata', {}).get('title'))
             for workspace in response.json().get('data', [])
         ]
-
+        self.store_in_session(request, 'get', True)
         self.store_in_session(request, 'workspace_choices', workspace_choices)
+        return redirect('project_export', self.get_from_session(request, 'project_id'), self.key)
 
-        form = self.Form(
-            dataset_choices=dataset_choices,
-            workspace_choices=workspace_choices
-        )
+    def get_post_url(self, workspace_id):
+        return '{}/radar/api/workspaces/{}/datasets'.format(self.radar_url, workspace_id)
 
-        return render(request, 'plugins/exports_radar.html', {'form': form}, status=200)
+    def get_post_data(self, set_index):
+        now = int(time.time())
+        email = self.request.user.email
+        dataset = self.get_dataset(set_index)
+
+        return {
+            'technicalMetadata': {
+                "retentionPeriod": 10,
+                "archiveDate": now,
+                "publishDate": now,
+                "responsibleEmail": email,
+                "schema": {
+                    "key": "RDDM",
+                    "version": "09"
+                }
+            },
+            'descriptiveMetadata': dataset
+        }
 
     def post_success(self, request, response):
         radar_id = response.json().get('id')
         if radar_id:
-            return redirect('{}/radar/de/dataset/{}'.format(self.radar_url, radar_id))
+            project_id = self.get_from_session(self.request, 'project_id')
+            set_index = self.get_from_session(self.request, 'set_index')
+
+            if request.LANGUAGE_CODE == 'de':
+                radar_url = '{}/radar/de/dataset/{}'.format(self.radar_url, radar_id)
+            else:
+                radar_url = '{}/radar/en/dataset/{}'.format(self.radar_url, radar_id)
+
+            try:
+                attribute = Attribute.objects.get(path='project/dataset/radar_id')
+                value, created = Value.objects.get_or_create(
+                    attribute=attribute,
+                    project_id=project_id,
+                    set_index=set_index
+                )
+                value.text = radar_id
+                value.save()
+            except Attribute.DoesNotExist:
+                pass
+
+            try:
+                attribute = Attribute.objects.get(path='project/dataset/radar_url')
+                value, created = Value.objects.get_or_create(
+                    attribute=attribute,
+                    project_id=project_id,
+                    set_index=set_index
+                )
+                value.text = radar_url
+                value.save()
+            except Attribute.DoesNotExist:
+                pass
+
+            return redirect(radar_url)
         else:
             return render(request, 'core/error.html', {
                 'title': _('RADAR error'),
@@ -100,48 +166,21 @@ class RadarExportProvider(RadarMixin, Export, OauthProviderMixin):
         return settings.RADAR_PROVIDER['client_secret']
 
     @property
-    def redirect_path(self):
-        return reverse('oauth_callback', args=['radar'])
-
-    def get_get_url(self):
-        return '{}/radar/api/workspaces'.format(self.radar_url)
-
-    def get_post_url(self, workspace_id):
-        return '{}/radar/api/workspaces/{}/datasets'.format(self.radar_url, workspace_id)
-
-    def get_post_data(self, set_index):
-        now = int(time.time())
-        email = self.request.user.email
-        dataset = self.get_dataset(set_index)
-
-        return {
-            'technicalMetadata': {
-                "retentionPeriod": 10,
-                "archiveDate": now,
-                "publishDate": now,
-                "responsibleEmail": email,
-                "schema": {
-                    "key": "RDDM",
-                    "version": "09"
-                }
-            },
-            'descriptiveMetadata': dataset
-        }
+    def redirect_uri(self):
+        return settings.RADAR_PROVIDER['redirect_uri']
 
     def get_authorize_params(self, request, state):
         return {
             'response_type': 'code',
             'client_id': 'jochenklar',
-            'redirect_uri': 'https://rdmo.jochenklar.dev/services/oauth/radar/callback/',
-            # 'redirect_uri': request.build_absolute_uri(self.redirect_path),
+            'redirect_uri': self.redirect_uri,
             'state': state
         }
 
     def get_callback_params(self, request):
         return {
             'grant_type': 'authorization_code',
-            # 'redirect_uri': request.build_absolute_uri(self.redirect_path),
-            'redirect_uri': 'https://rdmo.jochenklar.dev/services/oauth/radar/callback/',
+            'redirect_uri': self.redirect_uri,
             'code': request.GET.get('code')
         }
 
